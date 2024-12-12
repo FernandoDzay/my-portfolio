6d362b7094f95696a95a3f59017e5bf707489c96
FernandoDzay <luisf.dzay@gmail.com>
2024-12-11 20:08:20 -0600

    Terminando funcionalidad contacto

--- components/contactSection/index.tsx
+++ components/contactSection/index.tsx
@@ -4,24 +4,40 @@ import { slideIn } from "@/utils/motion"
 import Section from "../section"
 import { motion } from "framer-motion"
 import { styles } from "@/app/styles"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import EarthCanvas from "../earthCanvas"
 import StarsCanvas from "../starsCanvas"
+import contactAction from "./contactAction"
+import dynamic from 'next/dynamic'
+const SweetAlert2 = dynamic(() => import('@/components/sweetAlert'))
+const ReCAPTCHA = dynamic(() => import('react-google-recaptcha'))
 
 
 
 type Props = {}
 
 const ContactSection:React.FC<Props> = () => {
+    const form_key = 'massage_sent';
+    const [sweetAlertProps, setSweetAlertProps] = useState<{show: boolean, title?: string, text?: string, icon?: 'success'| 'error'}>({show: false});
+    const [downloadSweetAlert, setDownloadSweetAlert] = useState(false);
+    const [downloadReCaptcha, setDownloadReCaptcha] = useState(false);
     const [loading, setLoading] = useState(false);
+    const reCaptchaRef = useRef<any>();
     const [form, setForm] = useState({
-        name: "",
-        email: "",
-        message: "",
+        Nombre: '',
+        Email: '',
+        Mensaje: '',
     });
 
 
+    const setDownloadModulesTrue = () => {
+        setDownloadSweetAlert(true);
+        setDownloadReCaptcha(true);
+    }
+
     const handleChange = (e: any) => {
+        setDownloadModulesTrue();
+        setDownloadSweetAlert
         const { target } = e;
         const { name, value } = target;
     
@@ -31,10 +47,49 @@ const ContactSection:React.FC<Props> = () => {
         });
     };
 
-    const handleSubmit = (e: any) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        setDownloadModulesTrue();
         e.preventDefault();
-        setLoading(!loading);
-        console.log("submiting...");
+
+        if( localStorage.getItem(form_key) !== null ) {
+            setSweetAlertProps({show: true, icon: 'success', title: 'No te preocupes', text: 'Tu mensaje ya ha sido enviado, en breve me pondré en contacto contigo'});
+            setForm({
+                Nombre: '',
+                Email: '',
+                Mensaje: '',
+            });
+            return;
+        }
+
+        setLoading(true);
+        let reCaptchaToken: string|null = null;
+
+        if( process.env.NEXT_PUBLIC_ENABLE_RECAPTCHA === 'true' ) {
+            try {
+                if (!reCaptchaRef.current) throw new Error("No se configuró correctamente el reCaptcha");
+                reCaptchaToken = await reCaptchaRef.current.executeAsync();
+                reCaptchaRef.current.reset();
+            } catch(e: unknown) {
+                return setSweetAlertProps({show: true, icon: 'error', title: 'Ups', text: 'Ocurrió un error inesperado, en breve lo corrigiré para que no vuelva a suceder.'});
+            }
+        }
+
+        try {
+            const res = await contactAction({...form, reCaptchaToken});
+
+            if(!res.success) return setSweetAlertProps({show: true, icon: 'error', title: 'Ups', text: 'Ocurrió un error inesperado, en breve lo corrigiré para que no vuelva a suceder.'});
+            localStorage.setItem(form_key, 'true');
+        } catch(e: unknown) {
+            return setSweetAlertProps({show: true, icon: 'error', title: 'Ups', text: 'Ocurrió un error inesperado, en breve lo corrigiré para que no vuelva a suceder.'});
+        }
+
+        setSweetAlertProps({show: true, icon: 'success', title: 'Mensaje enviado', text: 'En breve me pondré en contacto contigo'});
+        setForm({
+            Nombre: '',
+            Email: '',
+            Mensaje: '',
+        });
+        setLoading(false);
     }
     
     
@@ -46,56 +101,70 @@ const ContactSection:React.FC<Props> = () => {
                         <p className={styles.sectionSubText}>Get in touch</p>
                         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-                        <form onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
+                        <form className='mt-12 flex flex-col gap-8' method="post" onSubmit={handleSubmit}>
                             <label className='flex flex-col'>
                                 <span className='text-white font-medium mb-4'>Your Name</span>
                                 <input
-                                type='text'
-                                name='name'
-                                value={form.name}
-                                onChange={handleChange}
-                                placeholder="What's your good name?"
-                                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-                                />
+                                    type='text'
+                                    name='Nombre'
+                                    minLength={4}
+                                    required
+                                    value={form.Nombre}
+                                    onChange={handleChange}
+                                    placeholder="What's your good name?"
+                                    className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                                    />
                             </label>
                             <label className='flex flex-col'>
                                 <span className='text-white font-medium mb-4'>Your email</span>
                                 <input
-                                type='email'
-                                name='email'
-                                value={form.email}
-                                onChange={handleChange}
-                                placeholder="What's your web address?"
-                                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                                    type='email'
+                                    name='Email'
+                                    required
+                                    value={form.Email}
+                                    onChange={handleChange}
+                                    placeholder="What's your web address?"
+                                    className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
                                 />
                             </label>
                             <label className='flex flex-col'>
                                 <span className='text-white font-medium mb-4'>Your Message</span>
                                 <textarea
-                                rows={7}
-                                name='message'
-                                value={form.message}
-                                onChange={handleChange}
-                                placeholder='What you want to say?'
-                                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                                    rows={5}
+                                    name='Mensaje'
+                                    required
+                                    minLength={4}
+                                    value={form.Mensaje}
+                                    onChange={handleChange}
+                                    placeholder='What you want to say?'
+                                    className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
                                 />
                             </label>
 
-                            <button type='submit' className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'>
+                            <button disabled={loading} type='submit' className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'>
                                 {loading ? "Sending..." : "Send"}
                             </button>
                         </form>
                     </motion.div>
 
-                    <motion.div
-                        variants={slideIn("right", "tween", 0.2, 1)}
-                        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-                    >
+                    <motion.div variants={slideIn("right", "tween", 0.2, 1)} className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
                         <EarthCanvas />
                     </motion.div>
                 </div>
             </div>
             <StarsCanvas />
+
+            {downloadSweetAlert && (
+                <SweetAlert2 {...sweetAlertProps} confirmButtonColor='#22344b' onConfirm={() => setSweetAlertProps({...sweetAlertProps, show: false})} />
+            )}
+            {process.env.NEXT_PUBLIC_ENABLE_RECAPTCHA === 'true' && downloadReCaptcha && (
+                <ReCAPTCHA
+                    sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ''}
+                    size="invisible"
+                    // @ts-ignore
+                    ref={reCaptchaRef} 
+                />
+            )}
         </Section>
     )
 }
